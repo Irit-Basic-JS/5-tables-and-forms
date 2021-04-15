ae9512e6054ab0b29075ff22728198c0720677e8
FreeTrally <39124135+FreeTrally@users.noreply.github.com>
2021-04-15 22:46:23 +0500

    Valid

---
+++ static/validation.js
@@ -0,0 +1,21 @@
+const phone = document.querySelector(".phone");
+const email = document.querySelector(".email");
+const submit = document.querySelector(".submit");
+
+function isEmailValid(email) {
+	// return email.includes("@") && email.includes(".");
+    const regex = /[.]@[.].[.]]/;
+	return email.match(regex);
+}
+
+function isNumberValid(number) {
+	const regex = /\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}/;
+	return number.match(regex);
+}
+
+submit.addEventListener("click", function (ev) {
+	if (!isEmailValid(email.value) || !isNumberValid(phone.value)) {
+		alert("Incorrect input");
+		ev.preventDefault();
+	}
+});
