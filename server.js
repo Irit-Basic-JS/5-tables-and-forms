f6d298435b2d72e34f65517793165b7e0ff607c4
ElizavetaUv <75488565+ElizavetaUv@users.noreply.github.com>
2021-11-11 01:09:23 +0500

    Добавила форму + добавила валидация для номера телефона

--- server.js
+++ server.js
@@ -33,15 +33,27 @@ app.listen(port, (err) => {
     console.log(`server is listening on ${port}`);
 });
 
+function isTelPhoneValid(telPhone) {
+    if (!telPhone) {
+        return false;
+    }
+    const regex = /\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}/;
+    return telPhone.match(regex);
+}
 
 app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/pets/orders', (request, response) => {
     const reqBody = request.body;
-    const pageBody = constructPageBody(reqBody);
 
-    console.log(request.body);
+    let pageBody = '';
+
+    if (!isTelPhoneValid(reqBody['phone'])) {
+        pageBody = '<h3 class="error">Неверный тип данных</h3><p>Поле "Ваш номер телефона" должно соответствовать ввиду +7 (000) 000-00-00<br>'
+    } else {
+        pageBody = constructPageBody(reqBody);
+    }
 
     response.send(`${pageHead}${pageBody}${pageFoot}`);
 });
